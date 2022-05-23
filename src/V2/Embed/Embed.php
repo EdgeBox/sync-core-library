275c45f21291b0d29572a49fc347647e3517f4fd
Thiemo Müller <thiemo@derthiemo.de>
2022-05-23 07:53:48 +0000

    fix(embed): lazy-load iframe can't rely on jquery being present already

--- src/V2/Embed/Embed.php
+++ src/V2/Embed/Embed.php
@@ -92,135 +92,144 @@ abstract class Embed
 <script type="text/javascript" src="'.$this->core->getCloudEmbedUrl().'/iframeResizer.js"></script>
 <script>
 (function() {
-  '.($is_line ? 'jQuery(document).ready( function() { document.getElementById("'.$id.'").src = "'.$this->url.'"; } );' : '').'
-
   // Avoid "mixed content" error message in case the base url is given as http but the currrent site is loaded via https.
   const getHttpsUrl = (url) => url.replace(/^https?:/, "");
   var listEntitiesUrl = getHttpsUrl("'.$list_entities_url.(false !== strpos($list_entities_url, '?') ? '&' : '?').'");
   var retrieveEntityUrl = getHttpsUrl("'.$retrieve_entity_url.(false !== strpos($retrieve_entity_url, '?') ? '&' : '?').'");
 
   var iframe = undefined;
+  function initIframe() {
+    iFrameResize({
+      //log: true,
+      checkOrigin: false,
+      onInit: function(newIframe) {
+        iframe = newIframe;
+        iframe.iFrameResizer.sendMessage({
+          type: "config",
+          config: '.json_encode($this->config).',
+        });
+        iframe.iFrameResizer.sendMessage({
+          type: "options",
+          options: '.json_encode($options).',
+        });
+      },
+      onMessage: function onMessage({message}) {
+        // Code provided by the parent application to add custom message handling.
+        '.$process_messages_javascript.'
 
-  iFrameResize({
-    //log: true,
-    checkOrigin: false,
-    onInit: function(newIframe) {
-      iframe = newIframe;
-      iframe.iFrameResizer.sendMessage({
-        type: "config",
-        config: '.json_encode($this->config).',
-      });
-      iframe.iFrameResizer.sendMessage({
-        type: "options",
-        options: '.json_encode($options).',
-      });
-    },
-    onMessage: function onMessage({message}) {
-      // Code provided by the parent application to add custom message handling.
-      '.$process_messages_javascript.'
-
-      // Need a fresh access token.
-      if(message.type==="reload") {
-        window.location.reload();
-      }
-      else if(message.type==="register-site") {
-        window.location.href = "'.$this->core->getApplication()->getEmbedBaseUrl(IEmbedService::REGISTER_SITE).'";
-      }
-      else if(message.type==="migrate") {
-        window.location.href = "'.$this->core->getApplication()->getEmbedBaseUrl(IEmbedService::MIGRATE).'";
-      }
-      else if(message.type==="migration-export-pools" || message.type==="migration-export-flows" || message.type==="migration-skip-flows-test" || message.type==="migration-skip-flows-push" || message.type==="migration-skip-flows-pull" || message.type==="migration-switch") {
-        var type = message.type.substr(10);
-        jQuery(`.migration-form #edit-${type} input`).prop("checked", false);
-        if(message.machineNames) {
-          for(var i=0; i<message.machineNames.length; i++) {
-            var machineName = message.machineNames[i];
-            jQuery(`.migration-form #edit-${type} input[value=${machineName}]`).prop("checked", true);
-          }
+        // Need a fresh access token.
+        if(message.type==="reload") {
+          window.location.reload();
         }
-        jQuery(`.migration-form #edit-action input`).prop("checked", false);
-        jQuery(`.migration-form #edit-action input[value=${type}]`).prop("checked", true);
-        jQuery(`.migration-form`).submit();
-      }
-      else if(message.type==="save-flow") {
-        jQuery(`.json-form #edit-json`).val(JSON.stringify(message.data));
-        jQuery(`.json-form #edit-action input`).prop("checked", false);
-        jQuery(`.json-form #edit-action input[value=${message.type}]`).prop("checked", true);
-        jQuery(`.json-form`).submit();
-      }
-      else if(message.type==="count-entities") {
-        onMessage({
-            message: {
-            ...message,
-            type: "list-entities",
-            page: 0,
-            itemsPerPage: 0,
-          },
-        });
-      }
-      else if(message.type==="list-entities") {
-        var {page, itemsPerPage, mode, namespaceMachineName, machineName, search, callbackId, flowMachineName} = message;
-        var params = [
-          `page=${page}`,
-          `itemsPerPage=${itemsPerPage}`,
-          `mode=${mode}`,
-          `namespaceMachineName=${namespaceMachineName}`,
-          `machineName=${machineName}`,
-          ...search ? [`search=${search}`] : [],
-        ];
-        var baseUrl = listEntitiesUrl
-          .replace(/\[flow\.machineName\]/g, flowMachineName||"'.IApplicationInterface::FLOW_NONE.'");
-        jQuery.ajax({
-          url: `${baseUrl}${params.join("&")}`,
-          method: "GET",
-          headers: {
-            "Accept": "application/json",
-          },
-          success: function(response, status, xhr) {
-            iframe.iFrameResizer.sendMessage({
-              type: "response",
-              callbackId,
-              response,
-            });
-          },
-        });
-      }
-      else if(message.type==="retrieve-entity") {
-        var {namespaceMachineName, machineName, sharedEntityId, callbackId, flowMachineName} = message;
-        var url = retrieveEntityUrl
-          .replace(/\[flow\.machineName\]/g, flowMachineName||"'.IApplicationInterface::FLOW_NONE.'")
-          .replace(/\[type\.namespaceMachineName\]/g, namespaceMachineName)
-          .replace(/\[type\.machineName\]/g, machineName)
-          .replace(/\[entity\.uuid\]/g, sharedEntityId)
-          .replace(/\[entity\.sharedId\]/g, sharedEntityId);
-        jQuery.ajax({
-          url,
-          method: "GET",
-          headers: {
-            "Accept": "application/json",
-          },
-          success: function(response, status, xhr) {
-            iframe.iFrameResizer.sendMessage({
-              type: "response",
-              callbackId,
-              response,
-            });
-          },
-        });
-      }
-      else if(message.type==="scroll-to-top") {
-        // Doesn\'t work in IE but that\'s alright.
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: "smooth",
-        });
-      }
-      else {
-        throw new Error("Unknown message "+JSON.stringify(message));
-      }
-    },
-  }, "#'.$id.'");
+        else if(message.type==="register-site") {
+          window.location.href = "'.$this->core->getApplication()->getEmbedBaseUrl(IEmbedService::REGISTER_SITE).'";
+        }
+        else if(message.type==="migrate") {
+          window.location.href = "'.$this->core->getApplication()->getEmbedBaseUrl(IEmbedService::MIGRATE).'";
+        }
+        else if(message.type==="migration-export-pools" || message.type==="migration-export-flows" || message.type==="migration-skip-flows-test" || message.type==="migration-skip-flows-push" || message.type==="migration-skip-flows-pull" || message.type==="migration-switch") {
+          var type = message.type.substr(10);
+          jQuery(`.migration-form #edit-${type} input`).prop("checked", false);
+          if(message.machineNames) {
+            for(var i=0; i<message.machineNames.length; i++) {
+              var machineName = message.machineNames[i];
+              jQuery(`.migration-form #edit-${type} input[value=${machineName}]`).prop("checked", true);
+            }
+          }
+          jQuery(`.migration-form #edit-action input`).prop("checked", false);
+          jQuery(`.migration-form #edit-action input[value=${type}]`).prop("checked", true);
+          jQuery(`.migration-form`).submit();
+        }
+        else if(message.type==="save-flow") {
+          jQuery(`.json-form #edit-json`).val(JSON.stringify(message.data));
+          jQuery(`.json-form #edit-action input`).prop("checked", false);
+          jQuery(`.json-form #edit-action input[value=${message.type}]`).prop("checked", true);
+          jQuery(`.json-form`).submit();
+        }
+        else if(message.type==="count-entities") {
+          onMessage({
+              message: {
+              ...message,
+              type: "list-entities",
+              page: 0,
+              itemsPerPage: 0,
+            },
+          });
+        }
+        else if(message.type==="list-entities") {
+          var {page, itemsPerPage, mode, namespaceMachineName, machineName, search, callbackId, flowMachineName} = message;
+          var params = [
+            `page=${page}`,
+            `itemsPerPage=${itemsPerPage}`,
+            `mode=${mode}`,
+            `namespaceMachineName=${namespaceMachineName}`,
+            `machineName=${machineName}`,
+            ...search ? [`search=${search}`] : [],
+          ];
+          var baseUrl = listEntitiesUrl
+            .replace(/\[flow\.machineName\]/g, flowMachineName||"'.IApplicationInterface::FLOW_NONE.'");
+          jQuery.ajax({
+            url: `${baseUrl}${params.join("&")}`,
+            method: "GET",
+            headers: {
+              "Accept": "application/json",
+            },
+            success: function(response, status, xhr) {
+              iframe.iFrameResizer.sendMessage({
+                type: "response",
+                callbackId,
+                response,
+              });
+            },
+          });
+        }
+        else if(message.type==="retrieve-entity") {
+          var {namespaceMachineName, machineName, sharedEntityId, callbackId, flowMachineName} = message;
+          var url = retrieveEntityUrl
+            .replace(/\[flow\.machineName\]/g, flowMachineName||"'.IApplicationInterface::FLOW_NONE.'")
+            .replace(/\[type\.namespaceMachineName\]/g, namespaceMachineName)
+            .replace(/\[type\.machineName\]/g, machineName)
+            .replace(/\[entity\.uuid\]/g, sharedEntityId)
+            .replace(/\[entity\.sharedId\]/g, sharedEntityId);
+          jQuery.ajax({
+            url,
+            method: "GET",
+            headers: {
+              "Accept": "application/json",
+            },
+            success: function(response, status, xhr) {
+              iframe.iFrameResizer.sendMessage({
+                type: "response",
+                callbackId,
+                response,
+              });
+            },
+          });
+        }
+        else if(message.type==="scroll-to-top") {
+          // Doesn\'t work in IE but that\'s alright.
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        }
+        else {
+          throw new Error("Unknown message "+JSON.stringify(message));
+        }
+      },
+    }, "#'.$id.'");
+  }
+
+  function onDocumentReady(clb) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(clb, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", clb);
+    }
+  }
+
+  '.($is_line ? 'onDocumentReady( function() { document.getElementById("'.$id.'").src = "'.$this->url.'"; initIframe(); } );' : 'initIframe();').'
 })();
 </script>';
 
